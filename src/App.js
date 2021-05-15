385244a85f21d48c08100f8846e00343c2ff881b
kaderDiarra <abdel-aziz-kader.diarra@epitech.eu>
2021-05-15 21:01:22 +0200

    cors

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import axios from 'axios'
 
 import Home from './Home'
 import Login from './authentification/Login'
 
 // https://dev.to/nburgess/creating-a-react-app-with-react-router-and-an-express-backend-33l3
 function App() {
+
+  useEffect(() => {
+    const getClients = async () => {
+      try {
+        const result = await axios.get('/client')
+        console.log("CLIENTS: ", result.data)
+      } catch (error) {
+        console.log("ERROR")
+      }
+    }
+    getClients()
+  }, [])
+
   return (
     <div className="App">
-      <Switch>
+      HELLO
+      {/*<Switch>
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
-      </Switch>
+      </Switch>*/}
     </div>
   );
 }
