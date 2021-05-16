eb6fdeda72b089c407ed543c7dcc0693398baee6
kaderDiarra <abdel-aziz-kader.diarra@epitech.eu>
2021-05-16 10:55:58 +0200

    edit proxy

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import axios from 'axios'
+
 import Home from './Home'
 import Login from './authentification/Login'
 
 // https://dev.to/nburgess/creating-a-react-app-with-react-router-and-an-express-backend-33l3
 function App() {
-
-  axios.defaults.proxy.protocol = 'https'
-  axios.defaults.proxy.host = 'malik-lbssociety-lbssociety-mirror-backend.zeet.app'
-  const handleClick = async () => {
-    try {
-      const result = await axios.get('/client')
-      console.log("CLIENTS: ", result.data)
-    } catch (error) {
-      console.log("ERROR")
-    }
-  }
-
   return (
     <div className="App">
-      <button onClick={handleClick}>HELLO</button>
-      {/*<Switch>
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
-      </Switch>*/}
+      </Switch>
     </div>
   );
 }
