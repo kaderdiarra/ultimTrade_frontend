1d62e10a02d9d6d7467a4bf5378995ee9c1130e4
Kader Diarra <kader_invictus@Kaders-MacBook-Pro.local>
2022-02-14 10:22:37 +0100

    [Edit] => pushing script

--- myPush.sh
+++ myPush.sh
@@ -4,7 +4,7 @@ branch_name=$(git symbolic-ref --short -q HEAD)
 green='\e[32m'
 end_color='\e[0m'
 
-if [ -z "$1" ]
+if [ -z "$1" ] || [ -z "$2" ]
 then 
     echo 'Inputs cannot be blank please try again!' 
     exit 0 
@@ -15,6 +15,6 @@ read -p "   " response
 
 if [[ "$response" == "y" ]]; then
     git add .
-    git commit -m "$1"
+    git commit -m "[$1] => $2"
     git push origin "$branch_name"
 fi
